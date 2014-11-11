adc0fdbfa6b53ffd97b021f80b4c1870dff1fccd
Gautham Warrier <gautham.r.w@gmail.com>
2014-11-11 22:29:56 +0530

    Update Boilerplate. Add OS X cache to gitignore

--- Project Euler/Boilerplate.cpp
+++ Project Euler/Boilerplate.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#define _ll long long
 #define Max(a, b) (a > b) ? a : b
 #define Min(a, b) (a < b) ? a : b
 #define gcd __gcd
@@ -10,6 +9,7 @@
 #define FastIO ios_base::sync_with_stdio(false); cin.tie(0);
 #define endl '\n'
 using namespace std;
+typedef long long ll;
 
 int main() {
 	FastIO
